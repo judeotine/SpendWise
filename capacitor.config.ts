e0361cc07d42d5b4ebc6571dadedfb203147e537
Jude_Otine <judextine28@gmail.com>
2025-04-08 19:43:06 +0300

    initialized capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jude.spendwise',
+  appName: 'spendwise',
+  webDir: 'dist'
+};
+
+export default config;
